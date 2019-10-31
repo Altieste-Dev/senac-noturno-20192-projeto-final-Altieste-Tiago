58b11d783ae4bb99047cd068721d2bcd1ef5f2ef
EMPRRFMNKND <43456160+EMPRRFMNKND@users.noreply.github.com>
2019-10-30 21:04:47 -0300

    Alterações nas Classes DAO. Implementações da interface DAO.

--- src/main/java/model/dao/VendasDAO.java
+++ src/main/java/model/dao/VendasDAO.java
@@ -1,5 +1,32 @@
 package model.dao;
 
-public class VendasDAO {
+import java.util.ArrayList;
+
+public class VendasDAO implements BaseDAO {
+
+	public Object salvar(Object novaEntidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean excluir(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean alterar(Object entidade) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object consultarPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList consultarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/model/dao/VendedorDAO.java
+++ src/main/java/model/dao/VendedorDAO.java
@@ -1,5 +1,32 @@
 package model.dao;
 
-public class VendedorDAO {
+import java.util.ArrayList;
+
+public class VendedorDAO implements BaseDAO {
+
+	public Object salvar(Object novaEntidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean excluir(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean alterar(Object entidade) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object consultarPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList consultarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
